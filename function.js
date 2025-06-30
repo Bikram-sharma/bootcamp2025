f40367315656723c06cad52e1073d5db94cb0ff9
bikram <sharmabikram061@gmail.com>
2025-06-30 12:30:38 +0600

    Adds replace function

--- function.js
+++ function.js
@@ -1,3 +1,15 @@
 function reverseString(str) {
   return str.split("").reverse().join("");
 }
+
+function replace(str, charToReplace, strReplacement) {
+  let result = "";
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] == charToReplace) {
+      result += strReplacement;
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
